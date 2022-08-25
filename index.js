cbb47ccb8247aaa794388620e4ad514979d78e31
nxoo <nxoo.dev@gmail.com>
2022-08-25 03:59:09 +0300

    test passed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(2,4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(x){
+    return function(fare){
+        return (Number(fare) * Number(x))
+    }
+}
+
+const fareCalculator = createFareMultiplier(2);
+function fareDoubler(fare){
+    return fareCalculator(fare)
+}
+
+const fareCalculator1 = createFareMultiplier(3);
+function fareTripler(fare){
+    return fareCalculator1(fare)
+}
+
+function selectDifferentDrivers(arrayOfDrivers, returnDriversFunctions) {
+    return returnDriversFunctions(arrayOfDrivers);
+}
+
